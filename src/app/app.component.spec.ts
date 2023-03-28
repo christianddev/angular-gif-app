62f842a1e4fce42a162008868b1bfea06c1052e0
Christian G.(WSL) <cdavid.garcia@gmail.com>
2023-03-28 17:20:38 +0200

    feat: update base app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gits-app'`, () => {
+  it(`should have as title 'gifs-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gits-app');
+    expect(app.title).toEqual('gifs-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gits-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gifs-app app is running!');
   });
 });
